c099208d434f934aee7b84aa923e6e372138643d
sotopro <dasg.1993@gmail.com>
2020-05-03 18:39:32 -0500

    [feature] - cssTransition & style components

--- semana5/src/Components/DropdownMenu/DropdownMenu.js
+++ semana5/src/Components/DropdownMenu/DropdownMenu.js
@@ -1,7 +1,11 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { ReactComponent as UserIcon } from '../../assets/icons/user.svg';
 import { ReactComponent as CogIcon } from '../../assets/icons/cog.svg';
+import { ReactComponent as ArrowIcon } from '../../assets/icons/arrow.svg';
+import { ReactComponent as BoltIcon } from '../../assets/icons/bolt.svg';
+import { ReactComponent as BellIcon } from '../../assets/icons/bell.svg';
 import './DropdownMenu.css';
+import { CSSTransition } from 'react-transition-group';
 
 export const DropdownMenu = (props) => {
 	const [activeMenu, setActiveMenu] = useState('main');
@@ -21,7 +25,7 @@ export const DropdownMenu = (props) => {
 			<a
 				href='#'
 				className='menu-item'
-				onClick={() => console.log('DropdownItem')}
+				onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}
 			>
 				<span className='icon-button'>{props.leftIcon}</span>
 				{props.children}
@@ -36,12 +40,44 @@ export const DropdownMenu = (props) => {
 			style={{ height: menuHeight }}
 			ref={dropdownRef}
 		>
-			<div className='menu'>
-				<DropdownItem leftIcon={<UserIcon />}>Mi Perfil</DropdownItem>
-				<DropdownItem leftIcon={<CogIcon />}>
-					Configuración
-				</DropdownItem>
-			</div>
+			<CSSTransition
+				in={activeMenu === 'main'}
+				timeout={500}
+				classNames='menu-primary'
+				unmountOnExit
+				onEnter={calHeight}
+			>
+				<div className='menu'>
+					<DropdownItem leftIcon={<UserIcon />}>
+						Mi Perfil
+					</DropdownItem>
+					<DropdownItem leftIcon={<CogIcon />} goToMenu='settings'>
+						Configuración
+					</DropdownItem>
+				</div>
+			</CSSTransition>
+			<CSSTransition
+				in={activeMenu === 'settings'}
+				timeout={500}
+				classNames='menu-secondary'
+				unmountOnExit
+				onEnter={calHeight}
+			>
+				<div className='menu'>
+					<DropdownItem goToMenu='main' leftIcon={<ArrowIcon />}>
+						<h3>Configuración</h3>
+					</DropdownItem>
+					<DropdownItem leftIcon={<BellIcon />}>
+						<span className='menu-name'>Notificaciones</span>
+					</DropdownItem>
+					<DropdownItem leftIcon={<BoltIcon />}>
+						<span className='menu-name'>Seguridad</span>
+					</DropdownItem>
+					<DropdownItem leftIcon={<BoltIcon />}>
+						<span className='menu-name'>Suscripciones</span>
+					</DropdownItem>
+				</div>
+			</CSSTransition>
 		</div>
 	);
 };
